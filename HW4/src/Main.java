c3eed68ee6aab06a166144bb3784a6361e9b62ed
Gremlin <velbyfr@gmail.com>
2023-12-12 20:33:22 +0300

    Добавлены условные операторы

--- HW4/src/Main.java
+++ HW4/src/Main.java
@@ -1,2 +1,26 @@
+import javax.swing.*;
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+//        < меньше
+//        > больше
+//        == равно
+//        != НЕ равно
+//        <= меньше или равно
+//        >= больше или равно
+//        && логическое И
+//        || логическое ИЛИ
+
+
+          int a = new Scanner(System.in).nextInt();
+
+          if (a < 5){
+              System.out.println("GG");
+          }
+          if (a >= 5){
+              System.out.println("gg");
+          }
+
+    }
 }
